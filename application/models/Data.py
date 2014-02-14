96307ed685c5135eb6de3e87be26334f7ff4217a
Deniz Celik <dcelik@TurkishDelight.(none)>
2014-02-13 21:58:18 -0500

    Added test function for model design; currently not working due to mongoengine import errors

--- application/models/Data.py
+++ application/models/Data.py
@@ -1,4 +1,5 @@
 from flask.ext.mongoengine import Document, EmbeddedDocument
+import time
 
 class Vector(EmbeddedDocument):
 	x = FloatField(required=True)
@@ -14,6 +15,14 @@ class DataPoint(EmbeddedDocument):
 	brake = FloatField()
 
 class DataSession(Document):
+	driver = StringField(required = True)
 	start_time = DateTimeField(required = True)
 	end_time = DateTimeField()
 	data = ListField(DataPoint)
+
+if __name__ == '__main__':
+	dummydata = []
+	for i in range(1,100):
+		datapoint = DataPoint(time=datetime.now())
+		dummydata.append(DataSession(driver = 'Patrick'+str(i),start_time = time.gmtime(([1392308290+(i*10)]))))
+	print str(dummydata)
