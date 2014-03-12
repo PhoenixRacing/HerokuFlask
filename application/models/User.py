49501efe5bdc3b178d3f62185261712bd17c9ad5
patrick <patrickjvarin@gmail.com>
2014-03-11 20:45:29 -0400

    styled the login and signup pages

--- application/models/User.py
+++ application/models/User.py
@@ -1,4 +1,9 @@
+from flask.ext.mongoengine import Document
 from .. import db
 
+access = ('admin','edit','none')
+
 class User(db.Document):
-	name = db.StringField(max_length=50, required=True)
+	first_name = db.StringField(max_length=50, required=True)
+	last_name = db.StringField(max_length=50, required=True)
+	access = db.StringField(default = 'none', required=True)
