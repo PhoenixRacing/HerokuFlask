c647de20da059a23d5d508dde4e935c8ecb002ef
patrick <patrickjvarin@gmail.com>
2014-03-13 18:06:22 -0400

    added forgotten files

---
+++ application/controllers/user.py
@@ -0,0 +1,21 @@
+from flask import render_template, request, redirect, url_for
+from flask.ext.login import current_user
+from ..models.forms import EditUserForm
+
+def user():
+	return render_template('user.html',user=current_user)
+
+def edit_user():
+	form = EditUserForm(request.form)
+	if request.method == 'POST' and form.validate():
+		if len(form.data['first_name']) > 0:
+			current_user.first_name = form.data['first_name']
+		if len(form.data['last_name']) > 0:
+			current_user.last_name = form.data['last_name']
+		if len(form.data['email']) > 0:
+			current_user.email = form.data['email']
+		current_user.save()
+		return redirect(url_for('user'))
+
+
+	return render_template('edit_user.html', form=form)
