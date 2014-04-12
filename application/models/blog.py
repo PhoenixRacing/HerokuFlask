1ac1b8d7d92b579e79c83f3b7dc566f69a3700e6
patrick <patrickjvarin@gmail.com>
2014-04-12 18:25:18 -0400

    fixed some styling

--- application/models/blog.py
+++ application/models/blog.py
@@ -6,12 +6,6 @@ class BlogPost(db.Document):
 	# TODO - include reference to the User that created the post
 	# TODO - include a timestamp for the creation of the post
 
-	@property
-	def p_list(self):
-	    return self.body.split('\n')
-
-# if BlogPost.objects().count() < 1:
-	# post = BlogPost()
-	# post.title = "Test Post"
-	# post.body = "This is the body of the post. This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.\nThis is the body of the post.This is the body of the post.This is the body of the post.\nThis is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post.This is the body of the post."
-	# post.save()
+	def preview(self):
+		# TODO - make this a preview of the blog post
+		return self.body
