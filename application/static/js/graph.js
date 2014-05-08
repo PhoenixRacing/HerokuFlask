203144f4b646b676988a490a875a67f36b8830f3
patrick <patrickjvarin@gmail.com>
2014-05-08 16:31:27 -0400

    forgotten commit

--- application/static/js/graph.js
+++ application/static/js/graph.js
@@ -1,24 +1,35 @@
 $(function () {
 
-    // unpack the time and speed variables
-    t = data.data.map(function(d){
-        if (d.speed != undefined){
-            return d.time.$date;
-        }
-    });
-    speed = data.data.map(function(d){
-        if (d.speed != undefined){
-            return d.speed;
-        }
-    });
+    if (data.data.length > 0){
+        initialize_chart(data);
+    }else{
+        $('#container').append('<p>Sorry, there is no data available at this time.</p>');
+    }
+
+});
+
+var max_data_points = 50;
+
+function initialize_chart(data){
+    $('#container').empty()
 
     // populate the chart with initial data
-    $('#container').highcharts({
+    chart = $('#container').highcharts({
         title: { text: 'Placeholder Graph'},
-        xAxis: { categories: t},
+        // xAxis: { categories: t},
         series: [{
-            data: speed,
+            data: getSpeedSeries(data),
             name: 'Speed'
         }]
     });
-});
+}
+
+function getSpeedSeries(data){
+    data_tuples = data.data.map(function(d){
+        if (d.speed != undefined){
+            dt = d.time.$date - data.start_time.$date;
+            return [dt/60000, d.speed];
+        }
+    });
+    return data_tuples.slice(-max_data_points)
+}
