851af3047969eea913fc350216d44e530e5d708d
dhellen <dhellen@ubuntu.ubuntu-domain>
2014-07-04 02:24:13 -0400

    Changed styling on sponsor page

---
+++ application/static/js/sponsors.js
@@ -0,0 +1,25 @@
+//TODO: Make this shit look better on mobile!
+$(document).ready(function(){
+	$('.gold-container').slick({
+		autoplay: true,
+		dots: true,
+		swipe: true,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 3,
+		slidesToScroll: 3
+	})
+});</script>
+
+<script>
+$(document).ready(function(){
+	$('.silver-container').slick({
+		autoplay: true,
+		dots: true,
+		swipe: true,
+		arrows: false,
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 5
+	})
+});
