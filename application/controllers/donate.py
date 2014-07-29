3c06e7bce6f069dd39132f54363c4646b356c95c
dhellen <dhellen@ubuntu.ubuntu-domain>
2014-07-29 04:01:13 -0400

    Reformatted home, subsystems, and donate page

--- application/controllers/donate.py
+++ application/controllers/donate.py
@@ -2,4 +2,4 @@ from flask import render_template
 from flask.ext.login import current_user
 
 def donate():
-	return render_template('donate.html', active_page='donate')
+	return render_template('donate_temp.html', active_page='donate')
--- application/controllers/subteams.py
+++ application/controllers/subteams.py
@@ -3,7 +3,7 @@ from ..models import subteam_list
 from flask.ext.login import current_user
 
 def subteams():
-	return render_template('subteams.html', active_page='subteams',subteams=subteam_list.values())
+	return render_template('subteams-temp.html', active_page='subteams',subteams=subteam_list.values())
 
 def description(team):
 	return render_template('subteamdescription.html', team=subteam_list[team], text=subteam_list[team].description.split('\n'))
