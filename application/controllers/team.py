975148f30662ca467ceaaea8699d2ab00e786d20
dhellen <dhellen@ubuntu.ubuntu-domain>
2014-10-11 20:11:28 -0400

    Major revisions to all pages

--- application/controllers/team.py
+++ application/controllers/team.py
@@ -1,7 +1,8 @@
 from flask import request, render_template
 from flask.ext.login import current_user
-from ..models import team_list
+from ..models import team_leads, general_mems, system_leads
 
 def team():
-	return render_template('team.html', active_page='team', team_members = team_list.values())
+	return render_template('team.html', active_page='team', \
+	general_members = general_mems.values(), team_leads=team_leads.values(), system_leads=system_leads.values())
 
