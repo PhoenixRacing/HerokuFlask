37a9b63d9c3550330b1b27b8f61670dfbcd4c9d5
dhellen <dhellen@ubuntu.ubuntu-domain>
2014-10-20 14:18:11 -0400

    More initial bug fixes

--- application/controllers/subteams.py
+++ application/controllers/subteams.py
@@ -3,7 +3,9 @@ from ..models import subteam_list
 from flask.ext.login import current_user
 
 def subteams():
-	return render_template('subteams.html', active_page='subteams',subteams=subteam_list.values())
+	sub_list = subteam_list.values()
+	sub_list.sort()
+	return render_template('subteams.html', active_page='subteams',subteams=sub_list)
 
 def description(team):
 	return render_template('subteamdescription.html', team=subteam_list[team], text=subteam_list[team].description.split('\n'))
--- application/controllers/team.py
+++ application/controllers/team.py
@@ -4,5 +4,6 @@ from ..models import team_leads, general_mems, system_leads
 
 def team():
 	return render_template('team.html', active_page='team', \
-	general_members = general_mems.values(), team_leads=team_leads.values(), system_leads=system_leads.values())
+	general_members = general_mems.values(), team_leads=team_leads.values(), \
+	system_leads=system_leads.values())
 
