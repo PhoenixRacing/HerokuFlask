e9c2373cbf03d1d94dab33a39765eb385217e61e
Kai Levy <kai0levy@gmail.com>
2014-10-22 01:21:53 -0400

    downlevel experience

--- application/static/js/index.js
+++ application/static/js/index.js
@@ -7,3 +7,25 @@ $(document).ready(function(){
 	$('.bottom-wrap').fadeIn(800);
 	$('.container').fadeIn('slow');
 });
+
+var ie = (function(){
+ 
+    var undef,
+        v = 3,
+        div = document.createElement('div'),
+        all = div.getElementsByTagName('i');
+ 
+    while (
+        div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+        all[0]
+    );
+ 
+    return v > 4 ? v : undef;
+ 
+}());
+if (ie<9){
+	document.write('<FONT SIZE="20">');
+	document.write('<a href="http://whatbrowser.org/">I&#39;m sorry! It appears that your browser is not compatible with our website! Please click HERE to get information about updating your software!</a>')
+	document.write('</FONT>')
+	document.execCommand('Stop');
+	}
